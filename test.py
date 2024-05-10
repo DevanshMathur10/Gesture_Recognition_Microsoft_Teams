c4cad99c36d68af9d70ff71373b2050a2cf57fe8
Devansh Mathur <drdevansh2003@gmail.com>
2024-05-10 22:23:11 +0530

    Reorganizing.

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-import string
-
-# Create a list of all digits
-digits = [str(i) for i in range(10)]
-
-# Create a list of all lowercase letters
-lowercase_letters = list(string.ascii_lowercase)
-
-# Create a list of all uppercase letters
-uppercase_letters = list(string.ascii_uppercase)
-
-# Combine all digits and letters into a single list
-all_digits_and_letters = digits + uppercase_letters
-
-print(all_digits_and_letters)
-print(len(all_digits_and_letters))
+#print("Devansh Mathur")
